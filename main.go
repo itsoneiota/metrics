c24924f479002416748aab256a78ed882f8cfa2b
owen morgan <owen.morgan@oneiota.co.uk>
2016-06-01 10:29:01 +0100

    init commit. support for incrementing.

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"time"
+
+	"github.com/cactus/go-statsd-client/statsd"
+)
+
+// Metrics interface
+type Metrics interface {
+	Inc(statName string, value int64)
+}
+
+// MetricClient - wrapper
+type MetricClient struct {
+	Client statsd.Statter
+}
+
+// MockMetricClient - wrapper
+type MockMetricClient struct {
+	MetricMap map[string]int64
+}
+
+// NewMetricsClient - prefx should be service name
+func NewMetricsClient(host string, prefix string) *MetricClient {
+	mtrcs, err := statsd.NewBufferedClient(host, prefix, 300*time.Millisecond, 0)
+	if err != nil {
+		panic("Error creating Metrics client.")
+	}
+	mclient := MetricClient{Client: mtrcs}
+	return &mclient
+}
+
+// NewMockMetricsClient - creates mock client
+func NewMockMetricsClient() *MockMetricClient {
+	mockClient := MockMetricClient{MetricMap: make(map[string]int64)}
+	return &mockClient
+}
+
+// Inc - increment metric
+func (mClient MetricClient) Inc(metricName string, value int64) {
+	mClient.Client.Inc(metricName, value, 1.0)
+}
+
+// Inc - increment mock metric
+func (mClient MockMetricClient) Inc(metricName string, value int64) {
+	mClient.MetricMap[metricName] += value
+}
---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestMockMetricTypeEquals(t *testing.T) {
+	client := NewMockMetricsClient()
+	client.Inc("TestMetric", 1)
+	assertEquals(1, client.MetricMap["TestMetric"], t)
+	client.Inc("TestMetric", 1)
+	assertEquals(2, client.MetricMap["TestMetric"], t)
+	client.Inc("TestMetric", 5)
+	assertEquals(7, client.MetricMap["TestMetric"], t)
+}
+
+func assertEquals(expected int64, have int64, t *testing.T) {
+	if expected != have {
+		t.Errorf("Expected metric value of %x, was %x", expected, have)
+	}
+}
